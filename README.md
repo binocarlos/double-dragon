a7c6563e91cce2029195b924dc1768c07464baab
Kai Davenport <kaiyadavenport@gmail.com>
2014-04-02 15:46:43 +0000

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,115 @@ double-dragon
 =============
 
 Sprite library for old skool stop frame animation made from a sequence of images
+
+Used for when a single sprite sheet is not an option.
+
+## installation
+
+```
+$ component install binocarlos/double-dragon
+```
+
+## usage
+
+```js
+var dd = require('double-dragon');
+
+var sprite = dd();
+
+// triggered when all images have pre-loaded
+sprite.on('loaded', function(){
+
+	// render the sprite
+	sprite.render(document.getElementById('spriteholder'));
+
+	// set the frames per second
+	sprite.fps(15);
+
+	// play it once
+	sprite.playOnce(function(){
+
+		// it has finished playing
+
+		// play repeatedly for 5 seconds
+		sprite.play();
+
+		setTimeout(function(){
+			sprite.stop();
+		}, 5000)
+	})
+})
+
+// will load 16 images from '/spriteimgs/sprite1.png' -> '/spriteimgs/sprite16.png'
+sprite.load({
+	imagepath:function(number){
+		return '/spriteimgs/sprite' + number + '.png';
+	},
+	from:1,
+	to:16
+})
+```
+
+## api
+
+### var sprite = dd()
+
+create a new sprite object
+
+### sprite.load(opts)
+
+load the images for the sprite - opts is an object:
+
+ * imagepath - a function that is passed the image index and should return the path to the image#
+ * from - the starting index for the sequence
+ * to - the ending index for the sequence
+
+### sprite.fps(frames_per_second)
+
+set the frames per second for the playback
+
+### sprite.playOnce(done)
+
+play the animation once and trigger the callback when finished
+
+### sprite.play()
+
+repeatedly play the animation
+
+### sprite.stop()
+
+stop the animation
+
+## events
+
+### sprite.on('progress', function(percent){})
+
+'progress' is emitted as the images are loaded - it will tell you the percentage complete
+
+### sprite.on('loaded', function(){})
+
+'loaded' is emitted once all images are loaded
+
+### sprite.on('render', function(){})
+
+'render' is emitted once the sprite has been rendered to a document element
+
+### sprite.on('play', function(){})
+
+'play' is emitted whenever the sprite beings to play
+
+### sprite.on('begin', function(){})
+
+'begin' is emitted whenever the sprite hits frame 1
+
+### sprite.on('frame', function(index){})
+
+'frame' is emitted whenever the sprite changes frame
+
+### sprite.on('stop', function(){})
+
+'stop' is emitted whenever the sprite stops playing
+
+## license
+
+MIT
---
+++ index.js
@@ -0,0 +1,155 @@
+var Preloader = require('preloader');
+var Emitter = require('emitter');
+
+
+function DoubleDragon(){
+	Emitter.call(this);
+	this._imgs = [];
+	this._currentindex = 0;
+	this.fps(30);
+}
+
+Emitter(DoubleDragon.prototype);
+
+DoubleDragon.prototype.fps = function(fps){
+	this._fps = fps || 1;
+	this._delay = 1000 / this._fps;
+}
+
+DoubleDragon.prototype.playOnce = function(direction, done){
+	var self = this;
+	if(arguments.length<=1){
+		done = direction;
+		direction = 1;
+	}
+
+	this.start(1, function(index){
+		return index<=self._imgs.length;
+	})
+}
+
+
+DoubleDragon.prototype.play = function(direction){
+	direction = direction || 1;
+
+	this.start();
+}
+
+DoubleDragon.prototype.addFrame = function(direction){
+	direction = direction || 1;
+	this._currentindex += direction;
+
+	if(direction>0){
+		if(this._currentindex>=this._imgs.length){
+			this._currentindex = 0;
+		}
+	}
+	else{
+		if(this._currentindex<0){
+			this._currentindex = this._imgs.length-1;
+		}
+	}
+}
+
+
+DoubleDragon.prototype.start = function(direction, fn){
+	var self = this;
+
+	fn = fn || function(){ return true; }
+	direction = direction || 1;
+
+	var totalcount = 0;
+
+	function nextframe(){
+		if(!fn(totalcount)){
+			return;
+		}
+
+		self._animtimeout = setTimeout(function(){
+			self.addFrame(direction);
+			totalcount++;
+			self.showFrame(self._currentindex);
+			nextframe();
+		}, self._delay);	
+	}
+
+	this.emit('play');
+
+	nextframe();
+	
+}
+
+DoubleDragon.prototype.stop = function(direction){
+	if(this._animtimeout){
+		clearTimeout(this._animtimeout);
+	}
+
+	this.emit('stop');
+}
+
+DoubleDragon.prototype.showFrame = function(index){
+	
+	if(this._visibleimg){
+		this._visibleimg.style.opacity = 0;
+	}
+	this._currentindex = index;
+	this._visibleimg = this._imgs[this._currentindex];
+	this._visibleimg.style.opacity = 1;
+
+	if(index==0){
+		this.emit('begin');
+	}
+	this.emit('frame', this._currentindex);
+}
+
+DoubleDragon.prototype.render = function(elem){
+
+	var self = this;
+
+	this._loader.urls.forEach(function(url){
+
+		var img = new Image;
+    img.src = url;
+    img.style.position = 'absolute';
+    img.style.opacity = 0;
+    self.emit('position', img);
+    elem.appendChild(img);
+		self._imgs.push(img);
+	})
+
+	this.emit('render', elem);
+
+	this.showFrame(0);
+	
+}
+
+DoubleDragon.prototype.load = function(opts){
+	var self = this;
+	opts = opts || {};
+	var from = opts.from || 0;
+	var to = opts.to || 0;
+	var imagepath = opts.imagepath || function(num){
+		return null;
+	}
+
+	this._loader = new Preloader;
+
+	for(var i=from; i<=to; i++){
+		var path = imagepath(i);
+		if(path){
+			this._loader.add(path);			
+		}
+	}
+
+	this._loader.on('progress', function(e){
+		self.emit('progress', e.percent);
+	})
+	
+	this._loader.end(function(){
+		self.emit('loaded');
+	})
+}
+
+module.exports = function(){
+	return new DoubleDragon();
+}
